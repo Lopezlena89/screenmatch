1e70f9e609b2651c79b194855f6115215ff8c36d
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2024-04-10 22:52:09 -0600

    first commit

---
+++ src/Principal.java
@@ -0,0 +1,24 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.nombre = "Encanto";
+        miPelicula.fechaDeLanzamiento = 2021;
+        miPelicula.duracionEnMinutos = 120;
+
+        miPelicula.nuestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+
+        System.out.println(miPelicula.sumaDeLasEvaluaciones);
+        System.out.println(miPelicula.totalDeLasEvaluaciones);
+        System.out.println(miPelicula.calculaMedia());
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "Matrix";
+        otraPelicula.fechaDeLanzamiento = 1999;
+        otraPelicula.duracionEnMinutos = 120;
+
+//        otraPelicula.nuestraFichaTecnica();
+    }
+}
