9c646ad519e3f912dd4d961d0916fe75f044aa16
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2024-04-12 22:39:00 -0600

    encapsulamiento

--- src/Principal.java
+++ src/Principal.java
@@ -1,23 +1,36 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
 public class Principal {
 
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre = "Encanto";
-        miPelicula.fechaDeLanzamiento = 2021;
-        miPelicula.duracionEnMinutos = 120;
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
 
         miPelicula.nuestraFichaTecnica();
         miPelicula.evalua(10);
         miPelicula.evalua(10);
+        miPelicula.evalua(7.8);
 
-        System.out.println(miPelicula.sumaDeLasEvaluaciones);
-        System.out.println(miPelicula.totalDeLasEvaluaciones);
+
+        System.out.println(miPelicula.getTotalDeLasEvaluaciones());
         System.out.println(miPelicula.calculaMedia());
 
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.nombre = "Matrix";
-        otraPelicula.fechaDeLanzamiento = 1999;
-        otraPelicula.duracionEnMinutos = 120;
+
+
+
+
+
+
+
+
+
+//        com.aluracursos.screenmatch.modelos.Pelicula otraPelicula = new com.aluracursos.screenmatch.modelos.Pelicula();
+//        otraPelicula.nombre = "Matrix";
+//        otraPelicula.fechaDeLanzamiento = 1999;
+//        otraPelicula.duracionEnMinutos = 120;
 
 //        otraPelicula.nuestraFichaTecnica();
     }
