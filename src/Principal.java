14f9d7dc690a3620f0235060e3d8cf2244e7a245
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2024-04-13 00:03:14 -0600

    polimorfismo

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
+import com.aluracursos.screenmatch.calculos.CalculadoraDeTiempo;
 import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
 
 public class Principal {
 
@@ -18,19 +20,29 @@ public class Principal {
         System.out.println(miPelicula.getTotalDeLasEvaluaciones());
         System.out.println(miPelicula.calculaMedia());
 
+        Serie casaDragon = new Serie();
+        casaDragon.setNombre("La casa del dragon");
+        casaDragon.setFechaDeLanzamiento(2022);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodio(50);
+        casaDragon.setEpisodiosPorTemporada(10);
+        casaDragon.nuestraFichaTecnica();
+        System.out.println(casaDragon.getDuracionEnMinutos());
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.setNombre("Matrix");
+        otraPelicula.setFechaDeLanzamiento(1999);
+        otraPelicula.setDuracionEnMinutos(180);
+
+        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+        calculadora.incluye(miPelicula);
+        calculadora.incluye(casaDragon);
+        calculadora.incluye(otraPelicula);
+        System.out.println("Tiempo necesario para ver tus titulos favoritos estas vacaciones " +
+                calculadora.getTiempoTotal() + " minutos");
 
 
 
-
-
-
-
-
-
-//        com.aluracursos.screenmatch.modelos.Pelicula otraPelicula = new com.aluracursos.screenmatch.modelos.Pelicula();
-//        otraPelicula.nombre = "Matrix";
-//        otraPelicula.fechaDeLanzamiento = 1999;
-//        otraPelicula.duracionEnMinutos = 120;
 
 //        otraPelicula.nuestraFichaTecnica();
     }
--- src/com/aluracursos/screenmatch/modelos/Pelicula.java
+++ src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -1,67 +1,14 @@
 package com.aluracursos.screenmatch.modelos;
 
-public class Pelicula {
-    private String nombre;
-    private int fechaDeLanzamiento;
-    private int duracionEnMinutos;
-    private boolean incluidoEnElPlan;
+public class Pelicula extends Titulo{
 
-    private double sumaDeLasEvaluaciones;
+    private String director;
 
-    private int totalDeLasEvaluaciones;
-
-    public String getNombre() {
-        return nombre;
+    public String getDirector() {
+        return director;
     }
 
-    public int getFechaDeLanzamiento() {
-        return fechaDeLanzamiento;
+    public void setDirector(String director) {
+        this.director = director;
     }
-
-    public int getDuracionEnMinutos() {
-        return duracionEnMinutos;
-    }
-
-    public boolean isIncluidoEnElPlan() {
-        return incluidoEnElPlan;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
-        this.fechaDeLanzamiento = fechaDeLanzamiento;
-    }
-
-    public void setDuracionEnMinutos(int duracionEnMinutos) {
-        this.duracionEnMinutos = duracionEnMinutos;
-    }
-
-    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
-        this.incluidoEnElPlan = incluidoEnElPlan;
-    }
-
-    public int getTotalDeLasEvaluaciones(){
-        return totalDeLasEvaluaciones;
-    }
-
-    public void nuestraFichaTecnica(){
-        System.out.println("El nombre de la pelicula es: " + nombre);
-        System.out.println("Su fecha de lanzamiento fue  " + fechaDeLanzamiento);
-        System.out.println("Duracion en minutos: " + duracionEnMinutos);
-    }
-
-    public void evalua(double nota){
-        sumaDeLasEvaluaciones += nota;
-        totalDeLasEvaluaciones++;
-    }
-
-
-    public double calculaMedia(){
-        return sumaDeLasEvaluaciones / totalDeLasEvaluaciones;
-    }
-
-
-
 }
